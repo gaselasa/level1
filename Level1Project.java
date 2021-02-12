5c92d8a03a4293bc5171c3dae0415233c209da53
gaselasa <gaselasa@gmail.com>
2021-02-12 15:58:54 -0800

    task two added

--- Level1Project.java
+++ Level1Project.java
@@ -16,7 +16,11 @@ public class Level1Project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("This two 73,5 number contains 3 "+checkNumberContain_3(73,5));
+           System.out.println("===================Task 1 ==================");
+           System.out.println("This two 73,5 number contains 3 "+checkNumberContain_3(73,5));
+        
+           System.out.println("===================Task 2 ==================");
+        System.out.println("check  two 73,5 number have  65 "+checkNumberContain_65(73,56));
     }
    static boolean checkNumberContain_3(int num1, int num2)
             {
@@ -39,6 +43,21 @@ public class Level1Project {
                 }
                 
                 return false;
+                
+            }
+
+
+static boolean checkNumberContain_65(int n1, int n2){
+                 
+                
+                int sum=n1+n2;
+             
+             
+                     if( n1==65 || n2==65 && sum==65)
+                         return true;
+                
+                return false;
+                
                 
             }   
 }
