2223212691b0bf42cf99959d7c96db8ec72d7a69
gaselasa <gaselasa@gmail.com>
2021-02-13 17:26:55 -0800

    added the last task 7

--- Level1Project.java
+++ Level1Project.java
@@ -9,6 +9,9 @@ package level1project;
  *
  * @author gasela
  */
+import java.util.ArrayList;
+import java.util.List;
+
 public class Level1Project {
 
     /**
@@ -16,144 +19,176 @@ public class Level1Project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-           
+
         System.out.println("===================Task 1 ==================");
-         sumOfNumbers();
+        sumOfNumbers();
         System.out.println("===================Task 2 ==================");
-           System.out.println("This two 73,5 number contains 3 "+checkNumberContain_3(73,5));
-        
-           System.out.println("===================Task 3 ==================");
-        System.out.println("check  two 73,5 number have  65 "+checkNumberContain_65(73,56));
-        
+        System.out.println("This two 73,5 number contains 3 " + checkNumberContain_3(73, 5));
+
+        System.out.println("===================Task 3 ==================");
+        System.out.println("check  two 73,5 number have  65 " + checkNumberContain_65(73, 56));
+
         System.out.println("===================Task 4 ==================");
-          square(6);
-          System.out.println("===================Task 5 ==================");
-          triangle(-6);
-             System.out.println("===================Task 6 ==================");
-              String [] listWord={"sanele","gaselaghg","horse","chickens"};
-                longest(listWord);
+        square(6);
+        System.out.println("===================Task 5 ==================");
+        triangle(-6);
+        System.out.println("===================Task 6 ==================");
+        String[] listWord = {"sanele", "gaselaghg", "horse", "chickens"};
+        longest(listWord);
+
+        System.out.println("===================Task 7 ==================");
+
+        List firstList = new ArrayList<>();
+        firstList.add(4);
+        firstList.add(5);
+        firstList.add(78);
+        firstList.add(9);
+        firstList.add(8);
+        List secondList = new ArrayList<>();
+        secondList.add(2);
+        secondList.add(8);
+        secondList.add(95);
+        secondList.add(7);
+        secondList.add(95);
+        secondList.add(7);
+
+        combineList(firstList, secondList);
     }
-    
-    
-    static void  sumOfNumbers(){
-        
-       int sum = 0;
-     for(int j=1;j<1000;j++){
-    if (j % 3 == 0 || j % 5 == 0)
-        sum = sum + j;
- 
+
+    private static void sumOfNumbers() {
+
+        int sum = 0;
+        for (int j = 1; j < 1000; j++) {
+            if (j % 3 == 0 || j % 5 == 0) {
+                sum = sum + j;
+            }
+
         }
-  System.out.println("The sum of natural numbers<1000 "+sum);
-  
-        
+        System.out.println("The sum of natural numbers<1000 " + sum);
+
     }
-   static boolean checkNumberContain_3(int num1, int num2)
-            {
-                int number=0;
-                
-                int sum=num1+num2;
-                  
-                    
-                while(sum>0){
-                    
-                     number=sum%10;
-                   
-                     
-                     if( num1==3 || num2==3 && number==3)
-                         return true;
-               
-                     
-                    sum =sum/10;
-            
+
+    private static boolean checkNumberContain_3(int num1, int num2) {
+        int number = 0;
+
+        int sum = num1 + num2;
+
+        while (sum > 0) {
+
+            number = sum % 10;
+
+            if (num1 == 3 || num2 == 3 && number == 3) {
+                return true;
+            }
+
+            sum = sum / 10;
+
+        }
+
+        return false;
+
+    }
+
+    private static boolean checkNumberContain_65(int n1, int n2) {
+
+        int sum = n1 + n2;
+
+        if (n1 == 65 || n2 == 65 && sum == 65) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    private static void square(int sides) {
+
+        for (int j = 0; j < sides; j++) {
+            for (int i = 0; i < sides; i++) {
+                System.out.print("#");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+    private static void triangle(int number) {
+
+        if (number > 0) {
+            for (int i = 0; i < number; i++) {
+
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("#" + " ");
                 }
-                
-                return false;
-                
+
+                System.out.println();
             }
 
+        } else {
 
-static boolean checkNumberContain_65(int n1, int n2){
-                 
-                
-                int sum=n1+n2;
-             
-             
-                     if( n1==65 || n2==65 && sum==65)
-                         return true;
-                
-                return false;
-                
-                
-            }
+            for (int j = number; j < 0; j++) {
 
-
-static void square(int sides){
-                
-                
-                
-                for(int j=0;j<sides;j++){
-                         for(int i=0;i<sides;i++)
-                       System.out.print("#");
-                         
-                         
-                    System.out.println();
-                    
+                for (int i = j; i < 0; i++) {
+                    System.out.print("#" + " ");
                 }
-                
-                
-                
+
+                System.out.println();
             }
 
-static void triangle(int number){
-                
-                if(number>0){
-                for(int i=0;i<number;i++){
-                    
-                     for(int j=0;j<=i;j++)
-                       System.out.print("#"+" ");
-                       
-                
-                     System.out.println();
-                }
-                
-                
-            }
-                else{
-                   
-                    for(int j=number; j<0; j++){
-                        
-                        for(int i=j;i<0;i++)
-                        System.out.print("#"+" ");
-                      
-                          
-                           System.out.println();
-                    }
-                    
-                    
-                    
-                }
-                
+        }
+
+    }
+
+    private static void longest(String[] listWords) {
+
+        int lengthWord = listWords[0].length();
+
+        for (int i = 1; i < listWords.length; i++) {
+
+            if (listWords[i].length() > lengthWord) {
+                lengthWord = listWords[i].length();
             }
 
-static void longest(String [] listWords){
-       
-       
-       
-       int lengthWord=listWords[0].length();
-       for(int i=1;i<listWords.length;i++){
-           
-           if(listWords[i].length()>lengthWord)
-               lengthWord=listWords[i].length();
-           
-       }
-       System.out.println(lengthWord);
-       
-      for(int i=0;i<listWords.length;i++){
-          if(listWords[i].length()==lengthWord)
-           System.out.println(listWords[i]);
-   }
-    
-   }
-    
+        }
+
+        for (int i = 0; i < listWords.length; i++) {
+
+            if (listWords[i].length() == lengthWord) {
+                System.out.println(listWords[i]);
+            }
+
+        }
+
+    }
+
+    private static void combineList(List firstList, List secontList) {
+
+        List newArray = new ArrayList();
+
+        int pos = 0;
+
+        int index = 0;
+
+        do {
+
+            if (index < firstList.size()) {
+                newArray.add(pos, firstList.get(index));
+            }
+
+            if (index < secontList.size()) {
+                newArray.add(pos + 1, secontList.get(index));
+            }
+
+            pos += 2;
+
+            index++;
+
+        } while (index < firstList.size() && index < secontList.size());
+
+        System.out.println(newArray);
+
+    }
 
 }
